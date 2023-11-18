3231ad7841dcfb1eef324b22db836aa2aafda130
Lam Ha <lam.ha@devsamurai.com>
2023-11-18 15:23:15 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
-import cors from 'cors';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  await app.listen(5019);
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'jwt',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'jwt', // This name should match the security name defined below
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
