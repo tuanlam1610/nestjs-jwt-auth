614fd0895dde0e1fdf8da2ffd544080fdaf1a0e4
Lam Ha <lam.ha@devsamurai.com>
2023-11-18 15:55:59 +0700

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    ,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
