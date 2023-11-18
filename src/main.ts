9d3982e717062991d0a4ebd3e19aca2177893d13
Lam Ha <lam.ha@devsamurai.com>
2023-11-18 15:29:32 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,25 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
-    .setVersion('0.1')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'jwt',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'jwt', // This name should match the security name defined below
-    )
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('Median')
+  //   .setDescription('The Median API description')
+  //   .setVersion('0.1')
+  //   .addBearerAuth(
+  //     {
+  //       type: 'http',
+  //       scheme: 'bearer',
+  //       bearerFormat: 'JWT',
+  //       name: 'jwt',
+  //       description: 'Enter JWT token',
+  //       in: 'header',
+  //     },
+  //     'jwt', // This name should match the security name defined below
+  //   )
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
