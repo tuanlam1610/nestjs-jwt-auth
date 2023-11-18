5ed1688912018a68285d27368b21ee8d53861b95
Lam Ha <lam.ha@devsamurai.com>
2023-11-18 15:34:21 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,6 @@ async function bootstrap() {
 
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(4000);
 }
